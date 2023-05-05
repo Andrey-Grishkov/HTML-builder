cf3f76ea81ca8e756852ad6b6a163682f579527e
Андрей Гришков <andreygorm@gmail.com>
2023-05-05 15:22:02 +0300

    feat: add fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(path.join(__dirname, "files-copy"), (err, files) => {
+  if (err) {
+    fs.mkdir(path.join(__dirname, 'files-copy'), err => {
+      if (err) throw err;
+    })
+  }
+
+if(files){
+  files.forEach(file => {
+    fs.unlink(path.join(__dirname, 'files-copy', file), err => {
+      if (err) throw err;
+    })
+  })
+}
+
+  fs.readdir(path.join(__dirname, "files"), (err, files) => {
+    if (err) throw err;
+
+    files.forEach(file => {
+
+      fs.readFile(path.join(__dirname, "files", file), (err, data) => {
+        if (err) throw err;
+
+        fs.writeFile(path.join(__dirname, "files-copy", file), data, (err) => {
+          if (err) throw err;
+        });})
+    })})
+})
+
 
