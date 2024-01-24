f6736ee2af0469630ef179733a6c4beb83fc538b
Андрей Гришков <andreygorm@gmail.com>
2024-01-24 14:09:21 +0300

    fix: fix 6 task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,194 +1,120 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
-//Копируем стили
-fs.readdir(path.join(__dirname, "styles"), (err, files) => {
-  if (err) throw err;
+async function copyStyles() {
+  try {
+    // Пытаемся прочитать файл styles.css
+    await fs.readFile(path.join(__dirname, "project-dist", "style.css"));
+  } catch (err) {
+    // Если файл не существует, создаем его
+    await fs.mkdir(path.join(__dirname, "project-dist"), { recursive: true });
+    await fs.writeFile(path.join(__dirname, "project-dist", "style.css"), '');
+  }
 
-  fs.readFile(path.join(__dirname, "project-dist", "styles.css"), (err, files) => {
+  // Очищаем файл styles.css
+  await fs.truncate(path.join(__dirname, "project-dist", "style.css"), 0);
 
-    //Создаем папку и фаил стилей в ней
-    if (err) {
-      fs.mkdir(path.join(__dirname, "project-dist"), err => {if (err) throw err;});
-      fs.writeFile(path.join(__dirname, "project-dist", "styles.css"), '', err => {
-        if (err) throw err;
-      })
+  // Собираем код из всех файлов стилей
+  const styleFiles = await fs.readdir(path.join(__dirname, "styles"));
+  for (const file of styleFiles) {
+    const fileStats = await fs.stat(path.join(__dirname, "styles", file));
+
+    if (fileStats.isFile() && path.extname(file) === ".css") {
+      const text = await fs.readFile(path.join(__dirname, "styles", file), "utf-8");
+      await fs.appendFile(path.join(__dirname, "project-dist", "style.css"), text);
+    }
+  }
+}
+
+async function createAssetsFolder() {
+  const assetsPath = path.join(__dirname, 'project-dist', 'assets');
+
+  try {
+    // Пытаемся получить статистику о папке assets
+    await fs.stat(assetsPath);
+  } catch (err) {
+    // Если папки нет, создаем её
+    await fs.mkdir(assetsPath, { recursive: true });
+  }
+
+  // Удаляем содержимое папки assets
+  const directories = await fs.readdir(assetsPath);
+  for (const dir of directories) {
+    const dirPath = path.join(assetsPath, dir);
+    const files = await fs.readdir(dirPath);
+
+    // Если в папке есть файлы, удаляем их
+    if (files.length > 0) {
+      for (const file of files) {
+        await fs.unlink(path.join(dirPath, file));
+      }
+    } else {
+      // Если в папке нет файлов, удаляем папку
+      await fs.rmdir(dirPath);
+      console.log(`Папка ${dirPath} удалена`);
+    }
+  }
+}
+
+async function copyAssetsFolders() {
+  const assetsPath = path.join(__dirname, 'project-dist', 'assets');
+  const sourceAssetsPath = path.join(__dirname, 'assets');
+
+  const directories = await fs.readdir(sourceAssetsPath);
+
+  await Promise.all(directories.map(async (dir) => {
+    const dirPath = path.join(sourceAssetsPath, dir);
+    const files = await fs.readdir(dirPath);
+
+    // Создаем подпапку и файлы в ней
+    const destDirPath = path.join(assetsPath, dir);
+    try {
+      // Пытаемся получить статистику о подпапке
+      await fs.stat(destDirPath);
+    } catch (err) {
+      // Если подпапки нет, создаем её
+      await fs.mkdir(destDirPath, { recursive: true });
     }
 
-    //Очищаем фаил стилей
-    fs.truncate(path.join(__dirname, "project-dist", "styles.css"), 0, err => {
-      if (err) throw err;
-    })
-  })
-
-  //Собираем код из всех фаилов стилей
-  files.forEach(file => {
-
-
-    //Проверяем формат всех фаилов
-    fs.stat(path.join(__dirname, "styles", file), (err, stats) => {
-      if (err) throw err;
-      if(path.extname(file) !== ".css"){
-
-      } else {
-
-        //Записываем все собранные стили в новый фаил
-        fs.readFile(path.join(__dirname, "project-dist", "styles.css"), (err, files) => {
-
-          if (err) throw err;
-
-          fs.readFile(path.join(__dirname, "styles", file), "utf-8", (err, text) => {
-            if (err) throw err;
-            fs.appendFile(path.join(__dirname, "project-dist", "styles.css"), text, err => {
-              if (err) throw err;
-            })
-          })
-        })
-      }
-    });
-  });
-})
-
-//Создаем папку assets (пытаемся узнать статистику по папке)
-fs.stat(path.join(__dirname, `project-dist\\assets`), (err, stat) => {
-  //Если такой папки нет, то создаем эту папку
-  if (err) {
-    fs.mkdir(path.join(__dirname, "project-dist\\assets"), err => {
-      if (err) throw err;
-    });
-  }
-})
-
-//Теперь удалим все содержимое папки, если оно есть
-fs.readdir(path.join(__dirname, "project-dist\\assets"), (err, directories) => {
-  if (err) throw err;
-
-if(directories){
-
-  //Для каждой подпапки
-  directories.forEach( dir => {
-
-    fs.readdir(path.join(__dirname, `project-dist\\assets\\${dir.toString()}`), (err, files) => {
-      if (err) throw err;
-
-      //Если в папке есть фаилы, то удаляем их
-      if(files){
-        files.forEach(file => {
-          fs.unlink(path.join(__dirname, `project-dist\\assets\\${dir.toString()}`, file), err => {
-            if (err) throw err;
-          })
-        })
-      }
-      //Если в папке нет фаилов, то удаляем папку
-      else {
-        console.log("privet")
-        fs.rmdir(path.join(__dirname, `project-dist\\assets\\${dir.toString()}`), (err) => {
-          if (err) throw err;
-        })
-      }
-    })
-  })
+    // Копируем файлы
+    await Promise.all(files.map(async (file) => {
+      const fileContent = await fs.readFile(path.join(dirPath, file));
+      await fs.writeFile(path.join(destDirPath, file), fileContent);
+    }));
+  }));
 }
-})
 
-//Копируем папки с картинками, шрифтами и т.д.
-//Залезаем в папку
-fs.readdir(path.join(__dirname, "assets"), (err, directories) => {
-  if (err) throw err;
+async function updateIndexHtml() {
+  const templatePath = path.join(__dirname, "template.html");
+  const indexPath = path.join(__dirname, "project-dist", "index.html");
 
-  //Для каждой подпапки
-  directories.forEach( dir => {
+  const [templateText, headerText, articlesText, footerText, aboutText] = await Promise.all([
+    fs.readFile(templatePath, "utf-8"),
+    fs.readFile(path.join(__dirname, "components", "header.html"), "utf-8"),
+    fs.readFile(path.join(__dirname, "components", "articles.html"), "utf-8"),
+    fs.readFile(path.join(__dirname, "components", "footer.html"), "utf-8"),
+    // Чтение about.html, если файл существует
+    fs.readFile(path.join(__dirname, "components", "about.html"), "utf-8").catch(() => ""),
+  ]);
 
-      //Залезаем в подпапку
-      fs.readdir(path.join(__dirname, `assets\\${dir.toString()}`), (err, files) => {
-        if (err) throw err;
+  // Заменяем теги в шаблоне
+  let indexText = templateText
+    .replace('{{header}}', headerText)
+    .replace('{{articles}}', articlesText)
+    .replace('{{footer}}', footerText)
+    .replace('{{about}}', aboutText);
 
-          //Для каждого фаила
-          files.forEach( file => {
+  await fs.writeFile(indexPath, indexText);
+}
 
-            //Создаем подпапки
-            fs.readFile(path.join(__dirname, `project-dist\\assets\\${dir.toString()}`, file), (err, files) => {
+// Вызываем функции
+async function main() {
+  await copyStyles();
+  await createAssetsFolder();
+  await copyAssetsFolders();
+  await updateIndexHtml();
+}
 
-              //Создаем подпапки и фаилы в них
-              if (err) {
+// Запускаем основную функцию
+main().catch((err) => console.error(err));
 
-
-                //Создаем подпапку (пытаемся узнать статистику по папке)
-                fs.stat(path.join(__dirname, `project-dist\\assets\\${dir.toString()}`), (err, stat) => {
-
-                  //Если такой папки нет, то создаем эту папку
-                  if (err) {
-                    fs.mkdir(path.join(__dirname, `project-dist\\assets\\${dir.toString()}`), err => {
-                      if (err) throw err;
-                    });
-                  }
-                })
-
-                  //Создаем фаилы в папке
-                fs.writeFile(path.join(__dirname, `project-dist\\assets\\${dir.toString()}`, file), '', err => {
-                  if (err) throw err;
-                })
-              }})
-
-            //Читаем текущий фаил и записываем его содержимый в новый
-            fs.readFile(path.join(__dirname, `assets\\${dir.toString()}`, file), "utf-8", (err, text) => {
-              if (err) throw err;
-              fs.appendFile(path.join(__dirname, `project-dist\\assets\\${dir.toString()}`, file), text, err => {
-                if (err) throw err;
-              })
-            })
-          });
-      })
-  })
-})
-
-//Читаем наш template
-fs.readFile(path.join(__dirname, "template.html"), "utf-8", (err, text) => {
-  if (err) throw err;
-
-  //Создаем новый html
-  fs.writeFile(path.join(__dirname, "project-dist", "index.html"), '', err => {
-    if (err) throw err;
-  })
-
-  fs.readFile(path.join(__dirname, "project-dist", "index.html"), (err, files) => {
-
-    if (err) throw err;
-
-      //Стираем все из фаила
-      fs.truncate(path.join(__dirname, "project-dist", "index.html"), 0, err => {
-        if (err) throw err;
-      })
-
-      //Добавляем новую инфу из индекса
-      fs.appendFile(path.join(__dirname, "project-dist", "index.html"), text.toString(), err => {
-        if (err) throw err;
-      })
-
-      //Теперь заменяем header
-      fs.readFile(path.join(__dirname, "components", "header.html"), "utf-8", (err, headerText) => {
-        if (err) throw err;
-
-        fs.readFile(path.join(__dirname, "components", "articles.html"), "utf-8", (err, articlesText) => {
-
-          if (err) throw err;
-
-          fs.readFile(path.join(__dirname, "components", "footer.html"), "utf-8", (err, footerText) => {
-            if (err) throw err;
-
-            fs.readFile(path.join(__dirname, "project-dist", "index.html"), "utf-8", (err, indexText) => {
-              if (err) throw err;
-
-              indexText = indexText.replace('{{header}}', headerText);
-              indexText = indexText.replace('{{articles}}', articlesText);
-              indexText = indexText.replace('{{footer}}', footerText);
-
-              fs.writeFile(path.join(__dirname, "project-dist", "index.html"), indexText, err => {
-                if (err) throw err;
-              })
-            })
-          })
-          })
-      })
-  })
-})
